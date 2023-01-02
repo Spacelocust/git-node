eb34b9bc1b202bb95c8508b8d360f1b73e4f8f8b
YummYume <givsoul@gmail.com>
2023-01-02 15:08:24 +0100

    Patch index.js

--- api/index.js
+++ api/index.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send('Hey, im a super powerfull 💪💪, blazingly fast 🚀🚀, node.js API 🔥🔥. 100% open source.');
 });
 
-const port = 5000; 
+const port = 5000;
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
